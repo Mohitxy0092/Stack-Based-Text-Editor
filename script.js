9cb86b43284885146e8cd87c10aab2ffa92350e0
Mohit <mohit952096@gmail.com>
2025-11-20 08:38:32 +0530

    Update script.js
    added feature

--- script.js
+++ script.js
@@ -1,54 +1,102 @@
 import { Stack } from './stack.js';
-document.onkeydown = function(event) {
-    if (event.ctrlKey || event.metaKey) {
-        event.preventDefault();
+
+window.addEventListener('load', function () {
+  const textbox = document.getElementById('comment');
+  const undo = document.getElementById('undo');
+  const clear = document.getElementById('clear');
+  const temptext = document.getElementById('temptext');
+  textbox.value = "";
+  let text = "";
+  let stack = new Stack();
+  document.addEventListener('keydown', function (event) {
+    const isUndo = (event.key === 'z' || event.key === 'Z') && (event.ctrlKey || event.metaKey);
+    if (isUndo) {
+      event.preventDefault();
     }
-};
-onload = function () {
-    const textbox = document.getElementById('comment');
-    const undo = document.getElementById('undo');
-    const clear = document.getElementById('clear');
-    const temptext = document.getElementById('temptext');
-
+  });
+  textbox.addEventListener('click', function () {
+    textbox.selectionStart = textbox.selectionEnd = textbox.value.length;
+  });
+  clear.addEventListener('click', function () {
+    stack.clear();
+    text = "";
     textbox.value = "";
-    let text = "";
-    let stack = new Stack();
-
-    textbox.onclick = function () {
-        textbox.selectionStart = textbox.selectionEnd = textbox.value.length;
-    };
-
-    clear.onclick = function () {
-        stack.clear();
-        text = "";
-        textbox.value = "";
-        temptext.innerHTML = "Sequence of operations will be here";
-    };
-
-    textbox.oninput = function(event){
-        switch(event.inputType){
-            case "insertText":
-                stack.push(0, event.data);
-                break;
-            case "deleteContentBackward":
-                stack.push(1, text[text.length-1]);
-                break;
-        }
-
-        temptext.innerHTML = "On stack "+stack.top()+"<br>"+temptext.innerHTML;
-    };
-
-    undo.onclick = function () {
-        let operation = stack.pop();
-        if(operation[0]!==-1){
-            temptext.innerHTML = "Performing undo operation<br>"+temptext.innerHTML;
-            if(operation[0] === 0){
-                let len = operation[1].length;
-                textbox.value = textbox.value.substring(0,textbox.value.length-len);
-            } else{
-                textbox.value += operation[1];
-            }
-            text = textbox.value;
-        }
-    };
-};
+    temptext.innerHTML = "Sequence of operations will be here";
+  });
+  textbox.addEventListener('input', function (event) {
+    const oldVal = text;
+    const newVal = textbox.value;
+    if (newVal.length > oldVal.length) {
+      const inserted = newVal.slice(
+        oldVal.length === textbox.selectionStart - (newVal.length - oldVal.length) 
+          ? oldVal.length 
+          : Math.min(oldVal.length, textbox.selectionStart - (newVal.length - oldVal.length))
+      );
+      const diff = newVal.split(oldVal).join('');
+      const toPush = diff.length ? diff : newVal.slice(oldVal.length);
+      stack.push(0, toPush);
+      temptext.innerHTML = "On stack " + stack.top() + "<br>" + temptext.innerHTML;
+    } else if (newVal.length < oldVal.length) {
+      let prefix = 0;
+      while (prefix < oldVal.length && prefix < newVal.length && oldVal[prefix] === newVal[prefix]) {
+        prefix++;
+      }
+      let suffix = 0;
+      while (
+        suffix < oldVal.length - prefix &&
+        suffix < newVal.length - prefix &&
+        oldVal[oldVal.length - 1 - suffix] === newVal[newVal.length - 1 - suffix]
+      ) {
+        suffix++;
+      }
+      const deleted = oldVal.substring(prefix, oldVal.length - suffix);
+      stack.push(1, deleted);
+      temptext.innerHTML = "On stack " + stack.top() + "<br>" + temptext.innerHTML;
+    } else {
+      let prefix = 0;
+      while (prefix < oldVal.length && oldVal[prefix] === newVal[prefix]) prefix++;
+      let suffix = 0;
+      while (
+        suffix < oldVal.length - prefix &&
+        oldVal[oldVal.length - 1 - suffix] === newVal[newVal.length - 1 - suffix]
+      ) suffix++;
+      const deleted = oldVal.substring(prefix, oldVal.length - suffix);
+      const inserted = newVal.substring(prefix, newVal.length - suffix);
+      if (deleted.length > 0) {
+        stack.push(1, deleted);
+      }
+      if (inserted.length > 0) {
+        stack.push(0, inserted);
+      }
+      temptext.innerHTML = "On stack " + stack.top() + "<br>" + temptext.innerHTML;
+    }
+    text = textbox.value;
+    const maxLines = 50;
+    const parts = temptext.innerHTML.split('<br>');
+    if (parts.length > maxLines * 2) {
+      temptext.innerHTML = parts.slice(0, maxLines * 2).join('<br>');
+    }
+  });
+  undo.addEventListener('click', function () {
+    const operation = stack.pop();
+    if (!operation || !Array.isArray(operation)) {
+      // nothing to undo
+      temptext.innerHTML = "Nothing to undo<br>" + temptext.innerHTML;
+      return;
+    }
+    temptext.innerHTML = "Performing undo operation<br>" + temptext.innerHTML;
+    const opType = operation[0];
+    const opPayload = operation[1] || "";
+    if (opType === 0) {
+      const len = opPayload.length;
+      if (len > 0) {
+        textbox.value = textbox.value.substring(0, textbox.value.length - len);
+      }
+    } else if (opType === 1) {
+      textbox.value += opPayload;
+    } else {
+      temptext.innerHTML = "Unknown operation on stack<br>" + temptext.innerHTML;
+    }
+    text = textbox.value;
+  });
+});
